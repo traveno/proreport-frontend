7d834f53aac0c2b143db84fcb022c08d5e87db6a
Stephen <teamgreensf@gmail.com>
2022-03-09 16:15:43 -0800

    Database service now speaks with ProData

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -1,18 +1,81 @@
 import { HttpClient } from '@angular/common/http';
-import { HtmlParser } from '@angular/compiler';
 import { Injectable } from '@angular/core';
-import $ from "cash-dom";
+import { Subject } from 'rxjs';
+import { PS_Database_Filter } from './proshop/Database';
+
+import * as ProData from './proshop/ProData';
+import { PS_WorkOrder, PS_WorkOrder_Status } from './proshop/WorkOrder';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
+  logEmitter = new Subject<string>();
+  dbStatusEmitter = new Subject<any>();
+  testdata = ['test data1', 'test data5', 'test data214'];
 
   constructor(private http: HttpClient) {
-    http.get('https://machinesciences.adionsystems.com', { responseType: 'text', observe: 'body' }).subscribe(data => {
-      const domParser = new DOMParser();
-      const htmlElement = domParser.parseFromString(data, 'text/html');
-      console.log($(htmlElement).find('span.user_name').text());
+    ProData.registerStatusUpdateCallback((data: ProData.PS_Status_Update) => this.statusCallback(data));
+  }
+
+  isInitialized(): boolean {
+    return ProData.isCacheInitialized();
+  }
+
+  getLogEmitter() {
+    return this.logEmitter;
+  }
+
+  addActivity() {
+    this.testdata.push(new Date().toDateString());
+  }
+
+  getMatchingWordOrders(options: PS_Database_Filter): PS_WorkOrder[] {
+    return ProData.getMatchingWorkOrders(options);
+  }
+
+  async loadDatabase(file: File) {
+    await ProData.loadDatabase(file);
+    this.dbStatusEmitter.next({
+      status: ProData.getDatabaseStatus(),
+      entries: ProData.getNumberOfEntries(),
+      timestamp: ProData.getDataTimestamp()
     });
+  }
+
+  statusCallback(data: ProData.PS_Status_Update) {
+    if (data.log)
+      this.logEmitter.next(data.log);
+    if (data.status)
+      this.logEmitter.next(data.status);
+  }
+
+  newDatabase() {
+    let options: ProData.PS_Update_Options = {
+      statuses: new Array(),
+      queries: new Array(),
+      machines: new Array(),
+      fetchExternal: true,
+      fetchInternal: true
+    };
+
+    // Status criteria
+
+    options.statuses.push(PS_WorkOrder_Status.ACTIVE);
+    options.statuses.push(PS_WorkOrder_Status.MANUFACTURING_COMPLETE);
+    options.statuses.push(PS_WorkOrder_Status.SHIPPED);
+    options.statuses.push(PS_WorkOrder_Status.ON_HOLD);
+    options.statuses.push(PS_WorkOrder_Status.CANCELED);
+    options.statuses.push(PS_WorkOrder_Status.INVOICED);
+    options.statuses.push(PS_WorkOrder_Status.COMPLETE);
+
+    // Department criteria
+    options.queries.push("query56");
+    options.queries.push("query55");
+    options.queries.push("query58");
+    options.queries.push("query57");
+    options.queries.push("query59");
+
+    ProData.newDatabase(options);
   }
 }
