c0cb20793b41eb034c529d74e6edb46751ec9b68
Stephen <teamgreensf@gmail.com>
2022-03-11 17:09:56 -0800

    Generate Report button implemented

--- src/app/fetch-controls/fetch-controls.component.ts
+++ src/app/fetch-controls/fetch-controls.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ConnectionsService } from '../connections.service';
 import { DatabaseService } from '../database.service';
 import { PS_Update_Options } from '../proshop/ProData';
+import { PS_WorkOrder_Status } from '../proshop/WorkOrder';
 
 @Component({
   selector: 'app-fetch-controls',
@@ -18,7 +20,7 @@ export class FetchControlsComponent implements OnInit {
 
   });
   statuses = new FormGroup({
-    active: new FormControl(false),
+    active: new FormControl(true),
     mfgcomplete: new FormControl(true),
     shipped: new FormControl(true),
     onhold: new FormControl(true),
@@ -32,17 +34,20 @@ export class FetchControlsComponent implements OnInit {
   });
 
 
-  constructor(public dbService: DatabaseService) { }
+  constructor(public dbService: DatabaseService, public connService: ConnectionsService) { }
 
   ngOnInit(): void {
-    console.log(this.statuses.getRawValue());
+  }
+
+  readyToSubmit(): boolean {
+    return this.dbService.isInitialized() && this.connService.getConnected();
   }
 
   submit() {
     let options: PS_Update_Options = {
-      statuses: new Array(),
-      queries: new Array(),
-      machines: new Array(),
+      statuses: [],
+      queries: [],
+      machines: [],
       fetchExternal: this.locations.getRawValue().proshop,
       fetchInternal: this.locations.getRawValue().database
     }
@@ -50,10 +55,49 @@ export class FetchControlsComponent implements OnInit {
     let deptValues = this.departments.getRawValue();
 
     if (deptValues.haas) {
-      options.queries.push();
-      options.machines.push()
+      options.queries.push('query56');
+      options.machines.push('HAAS')
     }
 
-    console.log(options);
+    if (deptValues.dmu) {
+      options.queries.push('query55');
+      options.machines.push('DMU')
+    }
+
+    if (deptValues.mam) {
+      options.queries.push('query58');
+      options.machines.push('MAM')
+    }
+
+    if (deptValues.mak) {
+      options.queries.push('query57');
+      options.machines.push('MAK')
+    }
+
+    if (deptValues.lathe) {
+      options.queries.push('query59');
+      options.machines.push("NL2500");
+      options.machines.push("NLX2500");
+      options.machines.push("NT1000");
+      options.machines.push("NTX2000");
+      options.machines.push("L2-20");
+    }
+
+    let statusValues = this.statuses.getRawValue();
+
+    if (statusValues.active)
+      options.statuses.push(PS_WorkOrder_Status.ACTIVE);
+    if (statusValues.mfgcomplete)
+      options.statuses.push(PS_WorkOrder_Status.MANUFACTURING_COMPLETE);
+    if (statusValues.shipped)
+      options.statuses.push(PS_WorkOrder_Status.SHIPPED)
+    if (statusValues.onhold)
+      options.statuses.push(PS_WorkOrder_Status.ON_HOLD)
+    if (statusValues.canceled)
+      options.statuses.push(PS_WorkOrder_Status.CANCELED);
+    if (statusValues.invoiced)
+      options.statuses.push(PS_WorkOrder_Status.INVOICED)
+
+    this.dbService.updateDatabase(options);
   }
 }
