8bd49151ff4ea11f28e2146b28c69110c534c1f7
Stephen <teamgreensf@gmail.com>
2022-03-15 17:44:48 -0700

    Background script imports definitions

--- src/background.js
+++ src/background.js
@@ -1,8 +1,16 @@
 chrome.action.onClicked.addListener(function() {
     chrome.tabs.create({
-        url: chrome.runtime.getURL("index.html"),
+        url: chrome.runtime.getURL('index.html'),
       }, function(win) {
         // win represents the Window object from windows API
         // Do something after opening
     });
+});
+
+// Pull company specific definitions for use in our angular application
+chrome.runtime.onInstalled.addListener(function(info) {
+  fetch(chrome.runtime.getURL('definitions.json')).then(res => res.text()).then(json => {
+    let def = JSON.parse(json);
+    chrome.storage.local.set({ definitions: def });
+  });
 });
