f9333d277f8243e2fdbf0e12152c288c17d2b739
Stephen <teamgreensf@gmail.com>
2022-03-31 15:55:45 -0700

    Refactor the way activity-overview fetches data

--- src/app/activity-overview/activity-overview.component.ts
+++ src/app/activity-overview/activity-overview.component.ts
@@ -13,33 +13,43 @@ import { PS_WorkOrder_Status } from '../proshop/WorkOrder';
 export class ActivityOverviewComponent implements OnInit {
   active = 0;
 
-  activityMap = new Map<string, PS_WorkOrder[]>();
-
   constructor(public dbService: DatabaseService, public defService: DefinitionsService,
               public apiService: ApiService) { }
 
   ngOnInit(): void {
-    for (let dept of this.defService.getDefinitions().departments) {
-      for (let machine of dept.machines) {
-        // this.apiService.getWorkOrdersByResource(machine).subscribe(results => {
-        //   this.activityMap.set(machine, results);
-        // });
-      }
-    }
+    // for (let dept of this.defService.getDefinitions().departments) {
+    //   for (let machine of dept.machines) {
+    //     // this.apiService.getWorkOrdersByResource(machine).subscribe(results => {
+    //     //   this.activityMap.set(machine, getResourceActivityByStatus(machine));
+    //     // });
+    //   }
+    // }
   }
 
   getResourceActivityByStatus(resource: string, status: PS_WorkOrder_Status): number {
+    let workOrderList = this.filterWorkOrdersByResource(resource);
     let temp = 0;
 
-    let resourceActivity = this.activityMap.get(resource);
-    if (resourceActivity === undefined) return -1;
-
-    for (let wo of resourceActivity) {
+    for (let wo of workOrderList) {
       if (wo.status === status)
         temp++;
     }
 
     return temp;
+  }
+
+  filterWorkOrdersByResource(resource: string): PS_WorkOrder[] {
+    let globalList = this.apiService.getWorkOrdersDetailedList();
+    let returnList: PS_WorkOrder[] = [];
+
+    for (let wo of globalList) {
+      for (let row of wo.routingRows) {
+        if (row.resource === resource)
+          returnList.push(wo);
+      }
+    }
+
+    return returnList;
   }
 
   generateActivityRow(machine: string): any {
